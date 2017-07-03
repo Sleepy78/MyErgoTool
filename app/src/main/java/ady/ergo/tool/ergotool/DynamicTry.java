8d5b90e4e2a69b469d2b1f0236702de0ea156cb3
Sleepy78 <aurelien78120@hotmail.com>
2017-07-04 00:49:53 +0200

    LinearLayout tries

--- app/src/main/java/ady/ergo/tool/ergotool/DynamicTry.java
+++ app/src/main/java/ady/ergo/tool/ergotool/DynamicTry.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,16 +17,21 @@ import java.util.ArrayList;
 
 public class DynamicTry extends AppCompatActivity {
     private RelativeLayout rl;
-    private ArrayList<Button> buttons;
-    private ArrayList<ToggleButton> togglebuttons;
-    private ArrayList<EditText> edittexts;
+    private ArrayList<Button> buttons,buttons2;
+    private ArrayList<ToggleButton> togglebuttons,togglebuttons2;
+    private ArrayList<EditText> edittexts,edittexts2;
     private ArrayList<RelativeLayout.LayoutParams> params;
+    private ArrayList<LinearLayout> ll2;
+
+    private LinearLayout ll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_try);
 
-        initObjects();
+        //initObjects();
+        initObjects2();
     }
     public View.OnClickListener listener = new View.OnClickListener() {
 
@@ -33,11 +39,79 @@ public class DynamicTry extends AppCompatActivity {
         public void onClick(View v) {
             int id = v.getId();
             //Toast.makeText(getApplicationContext(), String.valueOf(id), Toast.LENGTH_SHORT).show();
-            rl.removeView(v);
-            rl.removeView(edittexts.get(id - 1));
-            rl.removeView(togglebuttons.get(id - 1));
+            ll.removeView(ll2.get(id));
+
         }
     };
+
+    private void initObjects2() {
+        ll = (LinearLayout)findViewById(R.id.linearLayout);
+
+        buttons2 = new ArrayList<Button>();
+        togglebuttons2 = new ArrayList<ToggleButton>();
+        edittexts2 = new ArrayList<EditText>();
+        ll2 = new ArrayList<LinearLayout>();
+
+        edittexts2.add(new EditText(this));
+        edittexts2.get(0).setLayoutParams(new LinearLayout.LayoutParams(700, LinearLayout.LayoutParams.WRAP_CONTENT));
+        edittexts2.get(0).setId(0);
+        edittexts2.get(0).setText("new1");
+
+        buttons2.add(new Button(this));
+        buttons2.get(0).setLayoutParams(new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT));
+        buttons2.get(0).setId(0);
+        buttons2.get(0).setText("-");
+        buttons2.get(0).setOnClickListener(listener);
+
+        togglebuttons2.add(new ToggleButton(this));
+        togglebuttons2.get(0).setLayoutParams(new LinearLayout.LayoutParams(250, LinearLayout.LayoutParams.WRAP_CONTENT));
+        togglebuttons2.get(0).setId(0);
+
+
+        ll2.add(new LinearLayout(this));
+        ll2.get(0).setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams LLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        //ll2.setWeightSum(6f);
+        ll2.get(0).setLayoutParams(LLParams);
+
+        ll2.get(0).addView(edittexts2.get(0));
+        ll2.get(0).addView(togglebuttons2.get(0));
+        ll2.get(0).addView(buttons2.get(0));
+        ll.addView(ll2.get(0));
+
+
+        edittexts2.add(new EditText(this));
+        edittexts2.get(1).setLayoutParams(new LinearLayout.LayoutParams(700, LinearLayout.LayoutParams.WRAP_CONTENT));
+        edittexts2.get(1).setId(1);
+        edittexts2.get(1).setText("new2");
+
+        buttons2.add(new Button(this));
+        buttons2.get(1).setLayoutParams(new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT));
+        buttons2.get(1).setId(1);
+        buttons2.get(1).setText("-");
+        buttons2.get(1).setOnClickListener(listener);
+
+        togglebuttons2.add(new ToggleButton(this));
+        togglebuttons2.get(1).setLayoutParams(new LinearLayout.LayoutParams(250, LinearLayout.LayoutParams.WRAP_CONTENT));
+        togglebuttons2.get(1).setId(1);
+
+        ll2.add(new LinearLayout(this));
+        ll2.get(1).setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams LLParams2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        //ll2.setWeightSum(6f);
+        ll2.get(1).setLayoutParams(LLParams2);
+
+        ll2.get(1).addView(edittexts2.get(1));
+        ll2.get(1).addView(togglebuttons2.get(1));
+        ll2.get(1).addView(buttons2.get(1));
+        ll.addView(ll2.get(1));
+
+        //ll.removeView(ll2);
+
+
+        //ll.removeView(edittexts2.get(0));
+    }
+
     private void initObjects() {
         rl = (RelativeLayout)findViewById(R.id.relativeLayout);
 
